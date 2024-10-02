d998546b222c7ee8eb4320ad8377293b373ca31c
BenjaminJMcD <BenjaminJMcD@outlook.com>
2024-10-02 15:47:25 -0500

    build: array of objects with id, name, image url fetched from API

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import { useState } from 'react'
-
+import Card from './Card'
 
 function App() {
 
+  let poke="ditto"
+
+  return (
+    <div>
+      <Card />
+    </div>
+  )
 }
 
 export default App
--- src/Card.jsx
+++ src/Card.jsx
@@ -2,26 +2,40 @@ import React from "react";
 import { useEffect, useState } from "react";
 
 
-export default function Card() {
+function Card() {
 
-    const [image, setImage] = useState()
+    const pokemons = ['squirtle', 'ditto', 'blastoise']
+
+    const [pokemonImages, setPokemonImages] = useState([
+        {id: 1, name: "", url: ""}
+    ])
     
+    // useEffect(() => {
+    //     fetch(`https://pokeapi.co/api/v2/pokemon/ditto`)
+    //     .then(response => response.json())
+    //     .then(data => setImage(data.sprites.front_default))
+    // }, [])
+
     useEffect(() => {
-        fetch('https://pokeapi.co/api/v2/pokemon/ditto')
-        .then(response => response.json())
-        .then(data => setImage(data.sprites.front_default))
+        pokemons.map((pokemon) => {
+            fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+            .then(reponse => reponse.json())
+            .then(data => setPokemonImages((prev) => [...prev, { name: data.name, url: data.sprites.front_default }]))
+        })
     }, [])
 
     
+    console.log(pokemonImages)
 
 
     return (
         <div>
-            {image && <img src={image}></img>}
+
+
         </div>
     )
 
 }
 
 
-
+export default Card
