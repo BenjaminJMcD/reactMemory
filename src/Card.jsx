b0be699b54d07aa11f7d1f9238829dd9f8cb642f
BenjaminJMcD <BenjaminJMcD@outlook.com>
2024-10-03 13:06:29 -0500

    build: able to fetch all data for 9 API items

--- src/Card.jsx
+++ src/Card.jsx
@@ -6,9 +6,7 @@ function Card() {
 
     const pokemons = ['squirtle', 'ditto', 'blastoise']
 
-    const [pokemonImages, setPokemonImages] = useState([
-        {id: 1, name: "", url: ""}
-    ])
+    // const [pokemonUrl, setPokemonUrl] = useState(null)
     
     // useEffect(() => {
     //     fetch(`https://pokeapi.co/api/v2/pokemon/ditto`)
@@ -16,22 +14,54 @@ function Card() {
     //     .then(data => setImage(data.sprites.front_default))
     // }, [])
 
-    useEffect(() => {
-        pokemons.map((pokemon) => {
-            fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
-            .then(reponse => reponse.json())
-            .then(data => setPokemonImages((prev) => [...prev, { name: data.name, url: data.sprites.front_default }]))
-        })
-    }, [])
+    // useEffect(() => {
+    //     pokemons.map((pokemon) => {
+    //         fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+    //         .then(reponse => reponse.json())
+    //         .then(data => setPokemonUrl(data.sprites.front_default))
+    //     })
+    // }, [])
 
-    
-    console.log(pokemonImages)
+
+    const [items, setItems] = useState([])
+    const [isLoading, setIsLoading] = useState(false)
+
+    useEffect(() => {
+        const fetchData = async () => {
+            setIsLoading(true);
+            const response1 = await fetch('https://pokeapi.co/api/v2/pokemon/ditto')
+            const data1 = await response1.json()
+            const response2 = await fetch('https://pokeapi.co/api/v2/pokemon/pikachu')
+            const data2 = await response2.json()
+            const response3 = await fetch('https://pokeapi.co/api/v2/pokemon/mawile')
+            const data3 = await response3.json()
+            const response4 = await fetch('https://pokeapi.co/api/v2/pokemon/phantump')
+            const data4 = await response4.json()
+            const response5 = await fetch('https://pokeapi.co/api/v2/pokemon/haunter')
+            const data5 = await response5.json()
+            const response6 = await fetch('https://pokeapi.co/api/v2/pokemon/sandygast')
+            const data6 = await response6.json()
+            const response7 = await fetch('https://pokeapi.co/api/v2/pokemon/emolga')
+            const data7 = await response7.json()
+            const response8 = await fetch('https://pokeapi.co/api/v2/pokemon/charcadet')
+            const data8 = await response8.json()
+            const response9 = await fetch('https://pokeapi.co/api/v2/pokemon/kecleon')
+            const data9 = await response9.json()
+
+            const combinedData = [data1, data2, data3, data4, data5, data6, data7, data8, data9]
+            setItems(combinedData)
+        }
+        fetchData();
+        return () => {
+            setIsLoading(false);
+        }
+    }, []);
+
 
 
     return (
         <div>
-
-
+            
         </div>
     )
 
