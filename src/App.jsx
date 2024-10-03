87c5e6bc02c71c9e97edf0f673315494722c0fc5
BenjaminJMcD <BenjaminJMcD@outlook.com>
2024-10-03 13:18:45 -0500

    build: raised state up to App. all items fetch and render

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,52 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Card from './Card'
 
-function App() {
+export default function App() {
 
-  let poke="ditto"
+  const [items, setItems] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+
+  useEffect(() => {
+      const fetchData = async () => {
+          setIsLoading(true);
+          const response1 = await fetch('https://pokeapi.co/api/v2/pokemon/ditto')
+          const data1 = await response1.json()
+          const response2 = await fetch('https://pokeapi.co/api/v2/pokemon/pikachu')
+          const data2 = await response2.json()
+          const response3 = await fetch('https://pokeapi.co/api/v2/pokemon/mawile')
+          const data3 = await response3.json()
+          const response4 = await fetch('https://pokeapi.co/api/v2/pokemon/phantump')
+          const data4 = await response4.json()
+          const response5 = await fetch('https://pokeapi.co/api/v2/pokemon/haunter')
+          const data5 = await response5.json()
+          const response6 = await fetch('https://pokeapi.co/api/v2/pokemon/sandygast')
+          const data6 = await response6.json()
+          const response7 = await fetch('https://pokeapi.co/api/v2/pokemon/emolga')
+          const data7 = await response7.json()
+          const response8 = await fetch('https://pokeapi.co/api/v2/pokemon/charcadet')
+          const data8 = await response8.json()
+          const response9 = await fetch('https://pokeapi.co/api/v2/pokemon/kecleon')
+          const data9 = await response9.json()
+
+          const combinedData = [data1, data2, data3, data4, data5, data6, data7, data8, data9]
+          setItems(combinedData)
+      }
+      fetchData();
+      return () => {
+          setIsLoading(false);
+      }
+  }, []);
 
   return (
     <div>
-      <Card />
+      {items.map((item) => (
+        <Card 
+          image = {item.sprites.front_default}
+          name = {item.name}
+        />
+      ))}
     </div>
   )
 }
 
-export default App
+
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,10 +1,7 @@
 import React from "react";
-import { useEffect, useState } from "react";
 
 
-function Card() {
-
-    const pokemons = ['squirtle', 'ditto', 'blastoise']
+function Card({ image, name }) {
 
     // const [pokemonUrl, setPokemonUrl] = useState(null)
     
@@ -23,45 +20,14 @@ function Card() {
     // }, [])
 
 
-    const [items, setItems] = useState([])
-    const [isLoading, setIsLoading] = useState(false)
 
-    useEffect(() => {
-        const fetchData = async () => {
-            setIsLoading(true);
-            const response1 = await fetch('https://pokeapi.co/api/v2/pokemon/ditto')
-            const data1 = await response1.json()
-            const response2 = await fetch('https://pokeapi.co/api/v2/pokemon/pikachu')
-            const data2 = await response2.json()
-            const response3 = await fetch('https://pokeapi.co/api/v2/pokemon/mawile')
-            const data3 = await response3.json()
-            const response4 = await fetch('https://pokeapi.co/api/v2/pokemon/phantump')
-            const data4 = await response4.json()
-            const response5 = await fetch('https://pokeapi.co/api/v2/pokemon/haunter')
-            const data5 = await response5.json()
-            const response6 = await fetch('https://pokeapi.co/api/v2/pokemon/sandygast')
-            const data6 = await response6.json()
-            const response7 = await fetch('https://pokeapi.co/api/v2/pokemon/emolga')
-            const data7 = await response7.json()
-            const response8 = await fetch('https://pokeapi.co/api/v2/pokemon/charcadet')
-            const data8 = await response8.json()
-            const response9 = await fetch('https://pokeapi.co/api/v2/pokemon/kecleon')
-            const data9 = await response9.json()
-
-            const combinedData = [data1, data2, data3, data4, data5, data6, data7, data8, data9]
-            setItems(combinedData)
-        }
-        fetchData();
-        return () => {
-            setIsLoading(false);
-        }
-    }, []);
 
 
 
     return (
         <div>
-            
+            <img src={image} alt="" />
+            <h1>{name}</h1>
         </div>
     )
 
